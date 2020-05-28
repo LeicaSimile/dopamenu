f14798918a4296a245d4ab05377627718b86c5e2
Angelica Catalan <leicasimile@gmail.com>
2020-05-27 23:23:48 -0400

    Add ActivityList component

---
+++ src/app/activity-list/activity-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-list',
+  templateUrl: './activity-list.component.html'
+})
+export class ActivityListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActivityListComponent } from './activity-list/activity-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ActivityListComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
